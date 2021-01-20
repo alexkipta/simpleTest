3f80b40273fb8500daab45cdd5b392adf278f0df
BiancaMF <61747307+BiancaMF@users.noreply.github.com>
2021-01-20 11:17:31 +0200

    Update Calcs.java

--- src/test/java/Calcs.java
+++ src/test/java/Calcs.java
@@ -5,11 +5,11 @@ public class Calcs {
     @Test
     public void alwaysFail()
     {
-        Assert.assertEquals(1, 1);
+        Assert.assertEquals(1, 0);
     }
     @Test
     public void alwaysFail1()
     {
-        Assert.assertEquals(1, 2);
+        Assert.assertEquals(1, 1);
     }
 }
