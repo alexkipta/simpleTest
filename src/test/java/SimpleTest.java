1dff65e28107de2f165df4b9f60d71e308205a58
alexkipta <78084940+alexkipta@users.noreply.github.com>
2021-02-08 19:38:43 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -108,12 +108,12 @@ import java.util.Random;
                   @Test
                     public void alwaysPass6()
                     {
-                        Assert.assertEquals(1, 1);
+                        Assert.assertEquals(1, 2);
                     }
 			@Test
                     public void always1Pass6()
                     {
-                        Assert.assertEquals(1, 2);
+                        Assert.assertEquals(1, 1);
                     }
 			@Test
                     public void always2Pass6()
